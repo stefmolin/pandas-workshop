4ca17b3b6b88c9c2c7a48fba2a3fcca215373ca4
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-06-07 19:13:56 -0400

    Use uv for generating the slides

--- slides/generate_slides.sh
+++ slides/generate_slides.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-# setup
-VENV_NAME="pandas_workshop"
+#!/usr/bin/env -S uv run --group slides bash
 
 # get paths
 SCRIPT=$(realpath "$0")
@@ -14,40 +11,24 @@ if [[ "$#" -ne 1 ]]; then
 else
     TEMPLATE_TYPE="$1";
 
-    # get venv status
-    ACTIVE_ENV=$(basename $CONDA_DEFAULT_ENV)
+    # use nbmerge to combine all slide notebooks into a single notebook
+    echo "[nbmerge] Creating a combined notebook for all slides..."
+    COMBINED_NOTEBOOK="$SLIDES_DIR/workshop.ipynb"
+    nbmerge -o $COMBINED_NOTEBOOK $SLIDES_DIR/*.ipynb;
 
-    if [[ "$ACTIVE_ENV" == "" && "$CONDA_PREFIX" == "" ]]; then
-        echo "Virtual environment is not enabled. Quitting...";
-    else
-        if [[ "$ACTIVE_ENV" != "$VENV_NAME" && "$CONDA_PREFIX" != *"$VENV_NAME" ]]; then
-            echo "The $VENV_NAME conda env or venv is not activated.";
-        else
-            # if nbmerge isn't installed, do so
-            echo "Checking for nbmerge..."
-            pip3 freeze | grep nbmerge || pip3 install nbmerge;
+    # make all slide decks
+    jupyter nbconvert \
+        --to slides \
+        --template=$TEMPLATE_TYPE \
+        --TemplateExporter.extra_template_basedirs="$SLIDES_DIR"/templates \
+        --output-dir="$SLIDES_DIR"/html \
+        "$COMBINED_NOTEBOOK";
 
-            # use nbmerge to combine all slide notebooks into a single notebook
-            echo "[nbmerge] Creating a combined notebook for all slides..."
-            COMBINED_NOTEBOOK="$SLIDES_DIR/workshop.ipynb"
-            nbmerge -o $COMBINED_NOTEBOOK $SLIDES_DIR/*.ipynb;
+    # delete the combined notebook
+    echo "Cleaning up..."
+    rm $COMBINED_NOTEBOOK
 
-            # make all slide decks
-            jupyter nbconvert \
-                --to slides \
-                --template=$TEMPLATE_TYPE \
-                --TemplateExporter.extra_template_basedirs="$SLIDES_DIR"/templates \
-                --output-dir="$SLIDES_DIR"/html \
-                "$COMBINED_NOTEBOOK";
-
-            # delete the combined notebook
-            echo "Cleaning up..."
-            rm $COMBINED_NOTEBOOK
-
-            echo "Done!";
-        fi
-    fi
-
+    echo "Done!";
 fi
 
 
